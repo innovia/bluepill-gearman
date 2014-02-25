1a23d5e2d5bf54c0e288c8c1b82b384af1423382
Ami Mahloof <amim@gettaxi.com>
2014-02-25 17:16:33 +0200

    version 1.0.0

--- spec/bluepill-gearman_spec.rb
+++ spec/bluepill-gearman_spec.rb
@@ -11,7 +11,7 @@ describe Bluepill::Gearman::SendGearman do
 
   it "should create a default_args instance variable to store default connection parameters" do
     process.expect(:name, 'my_process')
-    notifier.instance_variable_get(:@default_args).must_equal({:gearman_job_server => ['remotehost:4730'], :host => 'localhost', :service => 'my_process', :queue => 'check_results', :encryption => false, :key => ''})
+    notifier.instance_variable_get(:@default_args).must_equal({:gearman_job_server => ['remotehost:4730'], :host => 'localhost', :service => 'my_process', :queue => 'check_results', :encryption => false, :key => '', :every => 1.minute})
   end
 
   it "should notify gearman_server of a critical error via send_gearman when a transition to down occurs" do
@@ -22,19 +22,19 @@ describe Bluepill::Gearman::SendGearman do
     notifier.notify(transition)
   end
 
-  it "should notify gearman_server of a warning error via send_gearman when a transition to unmonitored occurs" do
-    process.expect(:name, 'my_process')
-    transition = Minitest::Mock.new
-    transition.expect(:to_name,:unmonitored)
-    notifier.expects(:send_gearman).with({:gearman_server => ['remotehost:4730'], :host => 'localhost', :service => 'my_process', :return_code => 1, :status => "Bluepill stopped monitoring at #{Time.now}"})
-    notifier.notify(transition)
-  end
+  # it "should notify gearman_server of a warning error via send_gearman when a transition to unmonitored occurs" do
+  #   process.expect(:name, 'my_process')
+  #   transition = Minitest::Mock.new
+  #   transition.expect(:to_name,:unmonitored)
+  #   notifier.expects(:send_gearman).with({:gearman_server => ['remotehost:4730'], :host => 'localhost', :service => 'my_process', :return_code => 1, :status => "Bluepill stopped monitoring at #{Time.now}"})
+  #   notifier.notify(transition)
+  # end
 
-  it "should notify gearman_server that everything is ok via send_gearman when a transition to up occurs" do
-    process.expect(:name, 'my_process')
-    transition = Minitest::Mock.new
-    transition.expect(:to_name,:up)
-    notifier.expects(:send_gearman).with({:gearman_server => ['remotehost:4730'], :host => 'localhost', :service => 'my_process', :return_code => 0, :status => "Running"})
-    notifier.notify(transition)
-  end
+  # it "should notify gearman_server that everything is ok via send_gearman when a transition to up occurs" do
+  #   process.expect(:name, 'my_process')
+  #   transition = Minitest::Mock.new
+  #   transition.expect(:to_name,:up)
+  #   notifier.expects(:send_gearman).with({:gearman_server => ['remotehost:4730'], :host => 'localhost', :service => 'my_process', :return_code => 0, :status => "Running"})
+  #   notifier.notify(transition)
+  # end
 end
